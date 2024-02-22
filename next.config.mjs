f3cb5f4fed3c78c3a69463e27e356792bfb40b81
ricky <125274483+patrickNwafo@users.noreply.github.com>
2024-02-22 21:46:36 +0100

    tezt

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ["images.unsplash.com"]
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: "images.unsplash.com",
+                pathname: '**',
+            }
+
+        ]
+
     },
 };
 
